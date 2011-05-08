244b682672b23eff8965f93d99bedf96b22071a6
Sebastian Stadil <github@stadil.org>
2011-05-08 01:04:23 -0700

    finished demo app

--- demo/urls.py
+++ demo/urls.py
@@ -1,13 +1,10 @@
+# This also imports the include function
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	(r'^polls/$', 'polls.views.index'),
-	(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-	(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-	(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-	(r'^admin/', include(admin.site.urls)),
+    (r'^polls/', include('polls.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
