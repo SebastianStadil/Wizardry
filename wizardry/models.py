a251df10a0ebdc8e82524aee8f5ca16c68614e51
Quentin Pleplé <quentin.pleple@gmail.com>
2011-05-08 19:31:33 -0700

    required = false for trigger_id field in Wizard model

--- wizardry/models.py
+++ wizardry/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Wizard(models.Model):
 	target_id = models.CharField(max_length=20) # id attribute of the element to focus on
-	trigger_id = models.CharField(max_length=20) # id attribute of the element that triggers next step
+	trigger_id = models.CharField(max_length=20, required=False) # id attribute of the element that triggers next step
 	tooltip_title = models.CharField(max_length=70) # title of the tooltip
 	tooltip_text = models.CharField(max_length=500) # text to put in the body of the tooltip
 	def __unicode__(self):
