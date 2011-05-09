6137e62fc1d827537eccb32eae04629820891fda
Sebastian Stadil <github@stadil.org>
2011-05-08 20:14:19 -0700

    removed old code from urls.py

--- urls.py
+++ urls.py
@@ -18,7 +18,6 @@ urlpatterns = patterns('',
 	(r'^wizards/$', 'wizardry.views.index'),
 	(r'^wizards/(?P<wizard_id>\d+)/$', 'wizardry.views.detail'),
 	(r'^json/$', 'wizardry.views.json'),
-    (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': os.path.join(ROOT_PATH, 'static')}),
 
     # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
     # to INSTALLED_APPS to enable admin documentation:
