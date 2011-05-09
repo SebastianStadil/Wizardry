b491c3aa20869f13aaebf69e2a01b9de52914ccc
Sebastian Stadil <github@stadil.org>
2011-05-08 22:17:49 -0700

    changed mysql user to admin

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'wizardry_db',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
+        'USER': 'admin',                      # Not used with sqlite3.
         'PASSWORD': 'pladder',                  # Not used with sqlite3.
         'HOST': 'int-mysql.wizardryapp.com',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
